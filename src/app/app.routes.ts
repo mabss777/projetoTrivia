84befd75ee6310a5e0832d8b5bc579ca7bfd93e7
mabss777 <mabssdugrauu07@gmail.com>
2026-03-10 11:55:54 -0300

    Mudança nas rotas e HTML

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Quiz } from './pages/quiz/quiz';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
 
@@ -7,4 +8,16 @@ export const routes: Routes = [
     path: 'quiz',
     component: Quiz
   },
+  {
+    path: 'home',
+    component: Home
+  },
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'quiz/:difficulty',
+    component: Quiz
+  }
 ];
